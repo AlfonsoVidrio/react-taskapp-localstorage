817ef4ee59eafdcb77f65c877728411f2378c46f
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-01-02 00:16:41 -0600

    version 1

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import { TaskCreator } from "./components/TaskCreator";
+import { TaskTable } from "./components/TaskTable";
+import { VisibilityControl } from "./components/VisibilityControl";
+import { Container } from "./components/Container";
+
+function App() {
+  const [tasksItems, setTasksItems] = useState([]);
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  function createNewTask(taskName) {
+    //Si ese nombre de la tarea no existe, agregalo
+    if (!tasksItems.find((task) => task.name === taskName)) {
+      setTasksItems([...tasksItems, { name: taskName, done: false }]);
+    }
+  }
+
+  const toggleTask = (task) => {
+    setTasksItems(
+      // Si el nombre de la tarea coincide con el nombre de la tarea pasada como argumento, cambia su estado 'done'.
+      tasksItems.map((t) =>
+        t.name === task.name ? { ...t, done: !t.done } : t
+      )
+    );
+  };
+
+  useEffect(() => {
+    let data = localStorage.getItem("tasks");
+    if (data) {
+      setTasksItems(JSON.parse(data));
+    }
+  }, []);
+
+  const cleanTasks = () => {
+    setTasksItems(tasksItems.filter((task) => !task.done));
+    setShowCompleted(false);
+  };
+
+  // Si cambia el tasksItems se ejecuta el bloque de codigo
+  useEffect(() => {
+    //Guardamos todo el arrelgo en el local storage
+    localStorage.setItem("tasks", JSON.stringify(tasksItems));
+  }, [tasksItems]);
+
+  return (
+
+    <main className="bg-dark vh-100 text-white">
+   
+      <Container>
+      <h1>To Do List</h1>
+        <TaskCreator createNewTask={createNewTask} />
+
+        <TaskTable tasks={tasksItems} toggleTask={toggleTask} />
+
+        <VisibilityControl
+          isChecked={showCompleted}
+          setShowCompleted={(checked) => setShowCompleted(checked)}
+          cleanTasks={cleanTasks}
+        />
+        {showCompleted === true && (
+          <TaskTable
+            tasks={tasksItems}
+            toggleTask={toggleTask}
+            showCompleted={showCompleted}
+          />
+        )}
+      </Container>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/TaskRow.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const TaskRow = ({ task, toggleTask }) => {
+  return (
+    <tr key={task.name}>
+      <td className="d-flex justify-content-between">
+        {task.name}
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={() => toggleTask(task)}
+        ></input>
+      </td>
+    </tr>
+  );
+};
